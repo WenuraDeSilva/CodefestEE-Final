07a10eeb26f4a04b0a8be83bba924f9b43e0b74c
Wenura de Silva <79735156+wenuradesilva@users.noreply.github.com>
2021-04-06 14:35:22 +0530

    Initial Commit

---
+++ src/test/java/com/codefestday/Final/CodeFestDayEeFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefestday.Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFestDayEeFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
