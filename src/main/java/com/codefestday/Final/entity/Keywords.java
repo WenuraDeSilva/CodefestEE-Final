c122d00158e305365a658d327b2353089da97138
Wenura de Silva <79735156+wenuradesilva@users.noreply.github.com>
2021-04-06 21:28:50 +0530

    Update Keywords.java
    Removed unnecessary imports

--- src/main/java/com/codefestday/Final/entity/Keywords.java
+++ src/main/java/com/codefestday/Final/entity/Keywords.java
@@ -1,13 +1,9 @@
 package com.codefestday.Final.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
